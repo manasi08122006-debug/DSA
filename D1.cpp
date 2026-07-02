fa7033533db793965cbc298dfcd4ad511b2942a5
Manasi <manasi08122006@gmail.com>
2026-07-02 19:14:45 +0530

    Add D1 solution

---
+++ D1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hello, World!\n";
+    int a,b;
+    cout<<"Enter 1st numbers: ";
+    cin>>a;
+    cout<<"Enter 2nd numbers: ";
+    cin>>b;
+    cout<<"The sum is: "<<a+b;
+    return 0;
+
+}
